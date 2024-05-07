0d92cb811bb1b22e63f72ab359caa4ed6cec09e1
Bobes <robert.nadymacek@gmail.com>
2024-05-07 20:55:01 +0200

    Caliber
    Asks for the ammo type

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace GunHandling;
             Console.WriteLine("Velikost Zasobniku? :");
             a.MagazineSize = int.Parse(Console.ReadLine());
             
-            if (a.MagazineSize > 60 || a.MagazineSize < 1)
+            if (a.MagazineSize > 60 || a.MagazineSize < 1) //zkontroluje platnost zasobniku
             {
                 Console.WriteLine("Magazine Size Out of bounds, 1-60 max");
                 throw new ArgumentOutOfRangeException();
@@ -27,11 +27,39 @@ namespace GunHandling;
             {
                 bool ValidMagazine = true;
             }
-            
-
-
-            Thread.Sleep(3000);
+            Console.WriteLine(@"Typy Ráží:
+            1. 9mm
+            2. 5.56mm
+            3. 12 gauge
+            ");
+            string Caliber = Console.ReadLine();
+            if (Caliber == "9mm" || Caliber == "5.56mm" || Caliber == "12 gauge")
+            {
+                Console.WriteLine("Acceptable ammo Type");
+            }
+            else
+            {
+                Console.WriteLine("Invalid ammo type");
+                throw new ArgumentOutOfRangeException();
+            }
+            Console.ReadKey();
 
         }
     }
 
+/*
+// Ask for the user's favorite fruit
+var fruit = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("What's your [green]favorite fruit[/]?")
+        .PageSize(10)
+        .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+        .AddChoices(new[] {
+            "Apple", "Apricot", "Avocado", 
+            "Banana", "Blackcurrant", "Blueberry",
+            "Cherry", "Cloudberry", "Cocunut",
+        }));
+
+// Echo the fruit back to the terminal
+AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+*/
--- obj/Debug/net8.0/Zasobnik.AssemblyInfo.cs
+++ obj/Debug/net8.0/Zasobnik.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zasobnik")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00e709a1539316eafc81c25a075521f750dc1902")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6584d675b6c5bb7ca8ff2653f251c110e73a1a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zasobnik")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zasobnik")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
